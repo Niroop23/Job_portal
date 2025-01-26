d2570c5f2ff96f81bdc5d62cda46953dca7bde64
Niroop23 <shanmukhaniroop19@gmail.com>
2025-01-26 23:04:04 +0530

    footer component created

---
+++ frontend/jobportal/src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { FaPhoenixFramework } from "react-icons/fa";
+import { assets } from "../assets/assets";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="container px-4 2xl:px-16 mx-auto py-10 flex gap-4 items-center justify-between">
+        <div className="flex items-center gap-2">
+          <FaPhoenixFramework size={35} className="text-black" />
+          <span className="text-2xl font-semi-bold">NXTPATH</span>
+        </div>
+        <span className="flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden">
+          Copyright @NXTPATH | All rights reserved.
+        </span>
+        <div className="flex gap-4 items-center">
+          <img width={40} src={assets.facebook_icon} alt="" />
+          <img width={40} src={assets.instagram_icon} alt="" />
+          <img width={40} src={assets.twitter_icon} alt="" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
--- frontend/jobportal/src/pages/Home.jsx
+++ frontend/jobportal/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Nav from "../components/Nav";
 import Poster from "../components/Poster";
 import JobListings from "../components/JobListings";
 import PlayStore from "../components/PlayStore";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Poster />
       <JobListings />
       <PlayStore />
+      <Footer />
     </div>
   );
 };
