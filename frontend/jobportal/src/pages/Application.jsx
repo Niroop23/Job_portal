160b19230a3c08e8fdcb4ba7e78ec8a9cc5ac40e
Niroop23 <shanmukhaniroop19@gmail.com>
2025-03-08 01:36:20 +0530

    sstart

--- frontend/jobportal/src/pages/Application.jsx
+++ frontend/jobportal/src/pages/Application.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Nav from "../components/Nav";
 
 const Application = () => {
-  return <div>Application</div>;
+  return (
+    <>
+      <div>
+        <h2>Your Resume</h2>
+        <div></div>
+      </div>
+    </>
+  );
 };
 
 export default Application;
