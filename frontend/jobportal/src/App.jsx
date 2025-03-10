a4c9dcc6f0e3a433b0bb83143aa9016097d56919
Niroop23 <shanmukhaniroop19@gmail.com>
2025-03-10 22:48:38 +0530

    recruiter dashboard related pages setup

--- frontend/jobportal/src/App.jsx
+++ frontend/jobportal/src/App.jsx
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import JobApplication from "./pages/JobApplication";
 import Application from "./pages/Application";
 import Nav from "./components/Nav";
+import RecruiterLogin from "./components/RecruiterLogin";
+import { AppContext } from "./context/AppContext";
+import Dashboard from "./pages/Dashboard";
+import AddJob from "./pages/AddJob";
+import ManageJobs from "./pages/ManageJobs";
+import ViewApplication from "./pages/ViewApplication";
 
 const App = () => {
+  const { showRecruiterLogin } = useContext(AppContext);
   return (
     <>
       <div>
+        {showRecruiterLogin && <RecruiterLogin />}
         <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/apply/:id" element={<JobApplication />} />
           <Route path="/applications" element={<Application />} />
+          <Route path="/dashboard" element={<Dashboard />}>
+            <Route path="add-job" element={<AddJob />} />
+            <Route path="manage-jobs" element={<ManageJobs />} />
+            <Route path="view-applications" element={<ViewApplication />} />
+          </Route>
         </Routes>
       </div>
     </>
---
+++ frontend/jobportal/src/pages/Dashboard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const Dashboard = () => {
+  return <div className="min-h-screen "></div>;
+};
+
+export default Dashboard;
